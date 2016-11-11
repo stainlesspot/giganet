2365f750fb6bdfbeb6f256ade166108ab9ba0226
stainlesspot <deian.goranov@gmail.com>
2016-11-12 01:47:42 +0200

    File cleanup.

--- contract.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function (){
-    var networkLabel = $('.networkLabel'), networkInfo = $('.networkInfo');
-    networkLabel.css('left', (networkInfo.width() - networkLabel.width() - 5) / 2);
-    //networkLabel.css('top',-1 * (networkLabel.height()) / 2);
-});
